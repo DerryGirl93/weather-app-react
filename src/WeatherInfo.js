66764b1032f56470e3b5d95c2708f6f04e815ef3
DerryGirl93 <80162415+DerryGirl93@users.noreply.github.com>
2021-05-03 22:01:28 +0100

    Added Temperature conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,12 +1,13 @@
 import React from "react"
 import FormattedDate from './FormattedDate'
+import WeatherTemperature from "./WeatherTemperature"
 
 export default function WeatherInfo(props){
  return(
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <h2 className="text-capitalize">{props.data.description}</h2>
-      <h2 className="celcius">{Math.round(props.data.temperature)}°C | 68°F </h2>
+      <WeatherTemperature celsius={props.data.temperature}/>
       <img src={props.data.iconUrl} alt={props.data.description}/>
       <p>Last Updated: <FormattedDate date={props.data.date}/></p>
       <br />
